95a0df0e5313ae1c2fced522677e68b434c0d472
unknown <wwlee0405@gmail.com>
2021-06-07 18:56:10 +0900

    takePhoto

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -5,6 +5,7 @@ import styled from "styled-components/native";
 import {
   FlatList,
   Image,
+  StatusBar,
   TouchableOpacity,
   useWindowDimensions,
 } from "react-native";
@@ -96,6 +97,7 @@ export default function SelectPhoto({ navigation }) {
   );
   return (
     <Container>
+      <StatusBar />
       <Top>
         {chosenPhoto !== "" ? (
           <Image
